4d7186ee2467c23b5deccd54df002204346c4765
JingGai0524 <1530730178@qq.com>
2023-04-18 18:08:07 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import Othello.*;
+
+public final class Main {
+    private static void init() {
+        ImageValue.init();
+    }
+
+    public static void main(String[] args) {
+        init();
+
+        GameSystem.frame.setVisible(true);
+        GameSystem.frame.setPanel(new InitialPanel());
+//        GameSystem.frame.setPanel(new GamePanel());
+//        GameSystem.frame.setPanel(new ClassicChoose());
+//        GameSystem.frame.setPanel(new ComputerGame());
+        System.out.println("My name is Lzj\nI am 19");
+    }
+}
---
+++ src/Othello/test.java
@@ -0,0 +1,19 @@
+//package Othello;
+//
+//import java.util.Scanner;
+//
+//public class test {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner("iii lll");
+//        String a = input.next();
+//        String b = input.next();
+//        System.out.println(a);
+//        System.out.println(b);
+//        int i=1;
+//        if(i == 0)
+//            System.out.println();
+//            System.out.println();
+//        else
+//             System.out.println();
+//    }
+//}
